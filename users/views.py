8737ed74b5afd5da9ffa2c80dc0a890f72c1e193
mkaluuma9 <mkaluuma93@gmail.com>
2026-03-21 11:32:40 +0300

    Approval notifications

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import MultiPartParser, FormParser
 from django.contrib.auth import get_user_model
 from django.db import models
