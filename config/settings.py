ffe8e9403b65038db416b626e9be0e640dda8d4d
mkaluuma9 <mkaluuma93@gmail.com>
2026-04-17 20:30:59 +0300

    Payments worked on

--- config/settings.py
+++ config/settings.py
@@ -71,6 +71,17 @@ CHANNEL_LAYERS = {
     },
 }
 
+# 🌍 SHARED CACHE: This ensures Gunicorn and Daphne share heartbeats!
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 MIDDLEWARE = [
     # CORS middleware must be at the top
     'corsheaders.middleware.CorsMiddleware',
