b498e7abd37d25364cb952ce6b1d9c8b24b441e3
Tutu Melchizedek <tutumelchizedek8@gmail.com>
2026-05-09 13:35:44 +0000

    Bug fix

--- locations/admin_views.py
+++ locations/admin_views.py
@@ -1,10 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions
-from django.db.models import Q, Avg, Count
+from django.db.models import Q, Avg
 from .models import RodieLocation
-from requests.models import ServiceRequest, Rating
-from users.models import Wallet
+from requests.models import ServiceRequest
+from requests.models_rating import Rating
 
 
 class RealtimeLocationsView(APIView):
